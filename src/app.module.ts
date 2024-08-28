692bb913c70f7cbd3758ace1d710a01c6fc6f091
StillUsefull <109765950+StillUsefull@users.noreply.github.com>
2024-08-28 17:55:38 +0300

    FIX: add typeorm import in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DomainsModule } from './domains/domains.module';
 import { HttpModule } from './http/http.module';
+import {ProvidersModule} from "@lib/providers/providers.module";
 
 @Module({
-  imports: [DomainsModule, HttpModule],
+  imports: [DomainsModule, HttpModule, ProvidersModule],
   controllers: [],
   providers: [],
 })
